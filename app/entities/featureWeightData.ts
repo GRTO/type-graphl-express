61ba5dd4c8f28238f3fafb42bdd839685472a18a
Gerson Toribio <gerson.toribio@pucp.pe>
2020-05-19 15:03:50 -0500

    Adding JSON scalar

--- app/entities/featureWeightData.ts
+++ app/entities/featureWeightData.ts
@@ -1,8 +1,14 @@
 import { ObjectType, Field } from "type-graphql";
 import { getModelForClass } from "@typegoose/typegoose";
+import GraphQLJSON from "graphql-type-json";
 
 @ObjectType({ description: "The data feature weight model" })
 export class FeatureWeightData {
+  @Field(() => GraphQLJSON)
+  featureWeights: {
+    [key: string]: number;
+  };
+
   @Field()
   highThreshold: number;
 
--- app/utils/api-utils.ts
+++ app/utils/api-utils.ts
@@ -1,15 +1,15 @@
 import fetch, { RequestInit } from "node-fetch";
 // BaseURL
-export const BaseURL = "http://api.sp-qa-op.dev.sparkpredict.com:8000";
+export const BaseURL = "[BASE-URL]";
 
-const token = `eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJqdGkiOiJkMDViMDRhNC02ZGNkLTRmOWEtOTFjOS00ZDJkZjYzODk1MmEiLCJzdWIiOiIwMDBlOGVmNy0wN2ZiLTRjYTMtOWNkOC1hZDJkYmQ3YzQwYjciLCJzdWJfbmFtZSI6IkFsZXJ0IFVJIiwicm9sZXMiOlt7ImlkIjoiNjkwNzY2OWUtMjc1NS0xMWU5LTlkN2EtMGI5ZjA4MTdjMWEwIiwibmFtZSI6InVzZXIifV0sInBlcm1pc3Npb25zIjpbeyJpZCI6IjQ4ZDllZGE2LTI3NTUtMTFlOS05NTYzLWViOWZkZTM5MzcwNiIsIm5hbWUiOiJhbTpsb2dpbiJ9XSwiaXNzIjoiaHR0cDovLzM1LjE5Mi4xNDUuMjIwOjkwMDAiLCJleHAiOjE1ODk5MDk2MDAsImlhdCI6MTU4OTkwNjAwMCwidHlwIjoiQmVhcmVyIiwibmJmIjoxNTg5OTA1OTQwfQ.pp8SnCQBX2Gky94j2A4T1-hnf5QmG-6L4QFLUTFfP-vzynVPzOzIE1CjyJLIPw-r62YFmvA0B4kM5xg4kshaaXV_le29xuw6BcnnR_kgso_GRfJa0UyHNwVjGxBf1LkYfoCcHx_nrbJBcJj3QeyNHvHwgiZIoV-CdT4w7_IhBdutOAawXC7Rpbjo0e1WaYthpuSzLmxcs24K2Rr0X6mfW_pFPwv-DGPc1YRG7QhWGuPzbDHiTJ-McABucqNWgtCt0uUUNaUgdRMLMBTT0uKE768vZ8QF1-AuMDxPj5kDaGAIduxhOYrJqJI_DdmlzxE4gwny5YHCWYwOKxJOBuRurA`;
+const token = `[AUTH-HEADER]`;
 // Build the request header
 const requestHeader = () => {
   return {
     "Content-Type": "application/json",
     Authorization: `Bearer ${token}`,
     Cookie:
-      "session=f82049a7-edb0-407b-874c-8f70907ad02b.1lGWl3zEpKmJM90p1pfUOaU5z2M; locale=en",
+      "[COOKIE-SESSION]",
   };
 };
 
